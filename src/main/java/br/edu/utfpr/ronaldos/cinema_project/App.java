eab43ae3f18596b7c0cb016acb2d5088d2909198
rgoudinho <ronaldos@alunos.utfpr.edu.br>
2020-05-18 16:35:03 -0300

    primeiro commit

---
+++ src/main/java/br/edu/utfpr/ronaldos/cinema_project/App.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.ronaldos.cinema_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
